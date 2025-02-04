a5b485ebcf4561031739a0de8f02768ff572641b
fiopwk <da-juan3861@outlook.com>
2025-02-04 08:56:59 -0500

    added jakarta.persistence

--- app/src/main/java/org/nacha/domain/Payments.java
+++ app/src/main/java/org/nacha/domain/Payments.java
@@ -1,12 +1,19 @@
 package org.nacha.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 
 
 
+@Entity
 public class Payments {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
 
         private String currency;
         private LocalDateTime paymentDate;
