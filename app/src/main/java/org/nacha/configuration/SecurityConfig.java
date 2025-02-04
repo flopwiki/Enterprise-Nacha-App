1dc686924621ef682d75191aaaf3d83dd4467410
fiopwk <da-juan3861@outlook.com>
2025-02-04 11:21:41 -0500

    updated security config for user and admin access to payments

--- app/src/main/java/org/nacha/configuration/SecurityConfig.java
+++ app/src/main/java/org/nacha/configuration/SecurityConfig.java
@@ -1,3 +1,29 @@
 package org.nacha.configuration;
 
-public class SecurityConfig {}
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                // Payments
+                http
+                        .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/payments/**")
+                        .authenticated().anyRequest().permitAll());
+
+                // Users
+                http
+                        .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                                .anyRequest().authenticated());
+
+                
+                return http.build();
+        }
+}
